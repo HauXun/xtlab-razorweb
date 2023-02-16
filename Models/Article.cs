eafcd1378f347a22d57dcf0b0d9fd4c042645a56
Hau Xun <hautrieu65@gmail.com>
2023-02-16 11:12:02 +0700

    Tao trang CRUD, Article

--- Models/Article.cs
+++ Models/Article.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,16 +9,19 @@ namespace RazorWeb.Models
     [Key]
     public int Id { get; set; }
 
-    [StringLength(255)]
-    [Required]
+    [StringLength(255, MinimumLength = 5, ErrorMessage = "{0} phải dài từ {2} tới {1} kí tự")]
+    [Required(ErrorMessage = "{0} phải nhập")]
     [Column(TypeName = "nvarchar")]
+    [DisplayName("Tiêu đề")]
     public string Title { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "{0} phải nhập")]
     [DataType(DataType.DateTime)]
+    [DisplayName("Ngày tạo")]
     public DateTime Created { get; set; }
 
     [Column(TypeName = "ntext")]
+    [DisplayName("Nội dung")]
     public string Content { get; set; }
   }
 }
