2b109253026d0de4ce742ac23bcea50b1c78b737
Hauxun <hautrieu65@gmail.com>
2023-02-25 20:00:55 +0700

    update user profile

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -6,6 +6,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,107 +14,124 @@ using RazorWeb.Models;
 
 namespace RazorWeb.Areas.Identity.Pages.Account.Manage
 {
-    public class IndexModel : PageModel
+  [Authorize]
+  public class IndexModel : PageModel
+  {
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+
+    public IndexModel(
+        UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager)
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-
-        public IndexModel(
-            UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Username { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
-        {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
-        }
-
-        private async Task LoadAsync(AppUser user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
-            Username = userName;
-
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber
-            };
-        }
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
-            return RedirectToPage();
-        }
+      _userManager = userManager;
+      _signInManager = signInManager;
     }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public string Username { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class InputModel
+    {
+      /// <summary>
+      ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+      ///     directly from your code. This API may change or be removed in future releases.
+      /// </summary>
+      [Phone(ErrorMessage = "{0} sai định dạng")]
+      [Display(Name = "Số điện thoại")]
+      public string PhoneNumber { get; set; }
+
+      [Display(Name = "Địa chỉ")]
+      [StringLength(400)]
+      public string HomeAddress { get; set; }
+
+
+      [Display(Name = "Ngày sinh")]
+      public DateTime? BirthDate { get; set; }
+    }
+
+    private async Task LoadAsync(AppUser user)
+    {
+      var userName = await _userManager.GetUserNameAsync(user);
+      var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+      Username = userName;
+
+      Input = new InputModel
+      {
+        PhoneNumber = phoneNumber,
+        HomeAddress = user.HomeAddress,
+        BirthDate = user.BirthDate
+      };
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+      var user = await _userManager.GetUserAsync(User);
+      if (user == null)
+      {
+        return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+      }
+
+      await LoadAsync(user);
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      var user = await _userManager.GetUserAsync(User);
+      if (user == null)
+      {
+        return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+      }
+
+      if (!ModelState.IsValid)
+      {
+        await LoadAsync(user);
+        return Page();
+      }
+
+      //   var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+      //   if (Input.PhoneNumber != phoneNumber)
+      //   {
+      //     var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+      //     if (!setPhoneResult.Succeeded)
+      //     {
+      //       StatusMessage = "Unexpected error when trying to set phone number.";
+      //       return RedirectToPage();
+      //     }
+      //   }
+
+      user.HomeAddress = Input.HomeAddress;
+      user.BirthDate = Input.BirthDate;
+      user.PhoneNumber = Input.PhoneNumber;
+
+      await _userManager.UpdateAsync(user);
+
+      await _signInManager.RefreshSignInAsync(user);
+      StatusMessage = "Hồ sơ của bạn đã được cập nhập";
+      return RedirectToPage();
+    }
+  }
 }
---
+++ Migrations/20230225122954_UpdateUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomeAddress",
+                table: "Users",
+                type: "nvarchar(400)",
+                maxLength: 400,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HomeAddress",
+                table: "Users");
+        }
+    }
+}
